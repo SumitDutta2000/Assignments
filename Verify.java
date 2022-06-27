ac65a2de8d3e335b6345da77aada8ba3ea4e3a78
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-27 20:34:09 +0530

    Add files via upload

---
+++ Verify.java
@@ -0,0 +1,22 @@
+
+public class Verify {
+	
+	    public static void main(String[] args) 
+	    {
+	        int number=45;
+	        if(number > 0)
+	        {
+	            System.out.println(number+" is a positive number");
+	        }
+	        else if(number < 0)
+	        {
+	            System.out.println(number+" is a negative number");
+	        }
+	        else
+	        {
+	            System.out.println(number+" is zero");
+	        }
+	    }
+	}
+
+
