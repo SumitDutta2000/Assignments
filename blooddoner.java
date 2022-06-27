2fcbf5a93a6c2f0ebd7e53976cc019e6904a670e
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-27 20:26:08 +0530

    Add files via upload

---
+++ blooddoner.java
@@ -0,0 +1,18 @@
+
+public class blooddoner {
+	
+		 public static void main(String[] args) {
+			 int age=27;
+		     int weight=48;
+			 if(age>25 && weight>48)
+			 {
+		 System.out.println("eligible for donate blood");
+	         }
+			 else
+			 {
+				 System.out.println("not eligible for donate blood");
+			 }
+		 }
+	}
+
+
