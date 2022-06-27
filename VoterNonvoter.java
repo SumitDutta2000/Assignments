bec9b34fe4902c8b5c027993ba3958ac03ca46cd
SUMIT DUTTA <107988864+SumitDutta2000@users.noreply.github.com>
2022-06-27 20:30:05 +0530

    Add files via upload

---
+++ VoterNonvoter.java
@@ -0,0 +1,18 @@
+
+public class VoterNonvoter {
+	
+	    public static void main(String[] args)
+	    {
+	        int age=12;
+	        if(age>18)
+	        {
+	            System.out.println("eligible for vote.");
+	        }
+	        else
+	        {
+	            System.out.println("not eligible for vote.");
+	        }
+	    }
+	}
+
+
